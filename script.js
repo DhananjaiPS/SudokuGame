1cf997022f9d57c1d469f02fbbf6ec82814df6a5
dhananjai pratap singh <dhananjai@dhananjai-MacBook-M4.local>
2025-12-05 23:57:32 +0530

    UI board Design

---
+++ script.js
@@ -0,0 +1,59 @@
+
+let board = [
+  [5, 3, 0, 0, 7, 0, 0, 0, 0],
+  [6, 0, 0, 1, 9, 5, 0, 0, 0],
+  [0, 9, 8, 0, 0, 0, 0, 6, 0],
+  [8, 0, 0, 0, 6, 0, 0, 0, 3],
+  [4, 0, 0, 8, 0, 3, 0, 0, 1],
+  [7, 0, 0, 0, 2, 0, 0, 0, 6],
+  [0, 6, 0, 0, 0, 0, 2, 8, 0],
+  [0, 0, 0, 4, 1, 9, 0, 0, 5],
+  [0, 0, 0, 0, 8, 0, 0, 7, 9]
+];
+
+const boardDiv = document.getElementById("board");
+
+function drawBoard() {
+  boardDiv.innerHTML = "";
+  for (let r = 0; r < 9; r++) {
+    for (let c = 0; c < 9; c++) {
+      const cell = document.createElement("div");
+      cell.className = "cell";
+      if (board[r][c] !== 0) cell.classList.add("fixed");
+      cell.id = `cell-${r}-${c}`;
+      cell.textContent = board[r][c] !== 0 ? board[r][c] : "";
+      boardDiv.appendChild(cell);
+    }
+  }
+}
+
+drawBoard();
+
+document.getElementById("start").onclick = () => { solve(); };
+
+
+
+
+// same vaild function wriiten during interview 
+
+function isValid(r, c, num) {
+  for (let i = 0; i < 9; i++) {
+
+    if (board[r][i] === num) return false;
+    if (board[i][c] === num) return false;
+
+  }
+
+  const sr = Math.floor(r / 3) * 3;
+  const sc = Math.floor(c / 3) * 3;
+
+
+  for (let i = sr; i < sr + 3; i++)
+    for (let j = sc; j < sc + 3; j++)
+      if (board[i][j] === num) return false;
+
+
+  return true;
+}
+
+
